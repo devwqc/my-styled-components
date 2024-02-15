3dd20ff28e236e71d0c2c24fad0b75e87f09a5da
devwqc <devwqc@gmail.com>
2024-02-15 21:25:30 +0900

    feat: 다이나믹 스타일링

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import Button from './Button';
 function App() {
   return (
     <div>
-      <Button>Hello Styled!</Button>
+      <h1>스타일 기본</h1>
+      <Button.B1>Hello Styled!</Button.B1>
+      <h1>다이나믹 스타일링</h1>
+      <h2>기본</h2>
+      <Button.B2 size="small">size="small"</Button.B2>
+      <Button.B2 size="medium">size="medium"</Button.B2>
+      <Button.B2 size="large">size="large"</Button.B2>
+      <h2>둥근</h2>
+      <Button.B2 size="small" round>
+        size="small" round
+      </Button.B2>
+      <Button.B2 size="medium" round>
+        size="medium" round
+      </Button.B2>
+      <Button.B2 size="large" round>
+        size="large" round
+      </Button.B2>
     </div>
   );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,35 @@
 import styled from 'styled-components';
 
-const Button = styled.button`
+const SIZES = {
+  large: 24,
+  medium: 20,
+  small: 16,
+};
+
+const Button = styled.button``;
+
+const B1 = styled.button`
   background-color: #6750a4;
   border: none;
   color: #ffffff;
   padding: 16px;
 `;
+
+const B2 = styled.button`
+  background-color: #6750a4;
+  border: none;
+  border-radius: ${({ round }) => (round ? `9999px` : `3px`)};
+  color: #ffffff;
+  font-size: ${({ size }) => SIZES[size] ?? SIZES['medium']}px;
+  padding: 16px;
+
+  &:hover,
+  &:active {
+    background-color: #463770;
+  }
+`;
+
+Button.B1 = B1;
+Button.B2 = B2;
 
 export default Button;
